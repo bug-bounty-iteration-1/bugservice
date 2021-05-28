97246b6f789a3b6cb0a6678902484bf05bcef625
cmfabregas <cmfabregas1@gmail.com>
2021-05-28 17:30:02 -0400

    added create bug, return list of all bugs, updates to bug

--- src/main/java/com/bugbounty/repository/BugRepository.java
+++ src/main/java/com/bugbounty/repository/BugRepository.java
@@ -3,7 +3,22 @@ package com.bugbounty.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bugbounty.models.Bug;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface BugRepository extends JpaRepository<Bug, Integer>{
+
+    public List<Bug> findAll();
+
+    @Query("select b from Bug b JOIN b.bugOwner bo where bo.userId = userId")
+    public List<Bug> findAllFromUserId(int userId);
+
+    public Bug findByBugId(int bugId);
+
+
+
 
 }
--- src/main/java/com/bugbounty/service/BugService.java
+++ src/main/java/com/bugbounty/service/BugService.java
@@ -1,5 +1,7 @@
 package com.bugbounty.service;
 
+import com.bugbounty.models.Bug;
+import com.bugbounty.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,8 @@ import com.bugbounty.repository.RoleRepository;
 import com.bugbounty.repository.SolutionRepository;
 import com.bugbounty.repository.StatusRepository;
 import com.bugbounty.repository.UserRepository;
+
+import java.util.List;
 
 @Service
 public class BugService {
@@ -32,5 +36,25 @@ public class BugService {
 	public BugService() {
 
 	}
+
+	public List<Bug> getAllBugsByUserId(int userId){
+		return bugRepo.findAllFromUserId(userId);
+	}
+
+	public Bug updateBug(Bug bug)
+	{
+		return bugRepo.save(bug);
+	}
+
+	public int insertBug(Bug bug){
+		return bugRepo.save(bug).getBugId();
+	}
+
+	public List<Bug> getAllBugs(){
+		return bugRepo.findAll();
+	}
+
+
+
 
 }
